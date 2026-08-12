d96955dff6dcb2885e2d81da24c83bb6ae094d80
Gurutz Ocon <gurutz.ocon@gmail.com>
2026-08-12 09:44:30 +0200

    create function to encode trading data and create a unique positionId

--- src/AbiEncoderDemo.sol
+++ src/AbiEncoderDemo.sol
@@ -24,13 +24,37 @@ contract AbiEncoderDemo {
         address tokenA,
         address tokenB,
         uint24 fee
-    ) external pure returns(bytes32) {
+    ) external pure returns(bytes32 poolId) {
         // Token order matters for encoding. Our criteria is lowest token address first (token0).
         (address token0, address token1) = (tokenA < tokenB) ? (tokenA, tokenB) : (tokenB, tokenA);
 
         // Use of `abi.encodePacked`: Create a unique Pool Identifier
-        bytes32 poolId = keccak256(abi.encodePacked(token0, token1, fee));
-        return poolId
+        poolId = keccak256(abi.encodePacked(token0, token1, fee));
+    }
+
+    /**
+    * @dev encode trading position ID and its own data
+    * @param user user creating the position
+    * @param tokenIn token used to buy the token
+    * @param tokenOut token bought
+    * @param amountIn amount used to open the position
+    * @param minAmountOut the minimum amount accepted back
+    * @return positionId 
+    * @return encodedData
+    */
+    function encodeTradingPosition(
+        address user,
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 minAmountOut,
+        uint256 deadline
+    ) external pure returns(bytes32 positionId, bytes memory encodedData) {
+        // Encode the position data
+        encodedData = abi.encodePacked(user, tokenIn, tokenOut, amountIn, minAmountOut, deadline);
+
+        // Create a unique position ID
+        positionId = keccak256(encodedData);
     }
 
 }
